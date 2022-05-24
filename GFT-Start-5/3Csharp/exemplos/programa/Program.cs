4ab835c9d47cb8f638441a6ed57bedd759f5a99b
Pedrobolfute <pepenike-101@hotmail.com>
2022-05-24 16:49:12 -0300

    vazio

---
+++ GFT-Start-5/3Csharp/exemplos/programa/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+        
+            for(int i=1; numeroDeVezes >= i ; i++){
+                Console.WriteLine($"Bem vindo ao curso de dotnet {i}");
+            }
+        }
+    }
+}
