c0372d5f0d9d910439b10a7d0cf387d83ad5c929
Pedrobolfute <pepenike-101@hotmail.com>
2022-05-27 17:50:29 -0300

    Value Type comentado no código, que bagunça!

--- GFT-Start-5/3Csharp/exemplos/demo/Program.cs
+++ GFT-Start-5/3Csharp/exemplos/demo/Program.cs
@@ -3,15 +3,17 @@
 using System;
 using static System.Console;        // Assim não presiso de ficar usando o Console.WriteLine
 
+
 namespace demo  
 {
 
     /* Uma classe pública que mostra  */
     public class Program
     {   
-        static void Adicionar20(int a)                     /*   Isso é um Método (ele é um membro que fica contído no namespace. e na class.?.), e ajuda na class.
-                                                                "Ele não presisa ser puplico porque está na mesma classe"
-                                                            */
+        static void Adicionar20(int a)      /*Isso é um Método (ele é um membro que fica contído no namespace.
+                                             e na class.?.), e ajuda na class.
+                                            "Ele não presisa ser puplico porque está na mesma classe"
+                                            */
         {
             a = a + 20;     //no debug a variável "a" só vai ter o 20 somente *dentro desse método*
         }
@@ -21,11 +23,29 @@ namespace demo
         public static void Main(string[] args)
         {
             int a = 2;
-            Adicionar20(a); //Na chamada dessa ?função? aqui, mando adicionar 20 a minha variável "a" com o "intuito de printar 20 + 2= '22' no final".
-                            //Mas a ***a principal característica de um ValueType é que o valor de uma var é atribuido para outra var, ele é somente "copiado" e não é passado o valor.
-                            // e essa cópia só existe dentro da outra var da ?função? acima.***
-
+            Adicionar20(a); /*Na chamada dessa ?função? aqui, mando adicionar 20 a minha variável "a" com 
+                              o "intuito de printar 20 + 2= '22' no final".
+                              Mas a ***a principal característica de um ValueType é que o valor de uma var é atribuido
+                              para outra var, ele é somente "copiado" e não é passado o valor.
+                              e essa cópia só existe dentro da outra var da ?função? acima.***
+                            */
             WriteLine($"O valor da variável é {a}");
         }
     }
 }
+
+
+/*Pra concertar isso devemos dar uma meia volta nesse problema, mudando o meu método:
+
+static int Adicionar20(int a)   definindo o método como sendo um inteiro e teremos uma var inteira tbm
+{
+    return a + 20;                              retornar a variável + 20 la pra baixo e printa. 22
+}
+
+public static void Main()
+{
+    int a = 2;
+    a = Adicionar20(a);                         1) chama o método acima e o execulta {...}    2) valor retornado é guardada aqui na variável "a" e, então WriteLine
+    WriteLine($"O valor da variável é{a}");     printa...
+}
+*/
