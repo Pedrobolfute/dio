71175570be54d9e8e3073c3c7eea09de0eb043a6
Pedrobolfute <pepenike-101@hotmail.com>
2022-05-29 18:09:46 -0300

    desafio média2

---
+++ GFT-Start-5/3Csharp/exemplos/desafioMedia1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C;
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+           // if (A < 10 && B < 10 && C < 10)
+          //  {
+              double MEDIA = ((A*2) + (B*3) + (C*5))/10;
+              Console.WriteLine($"MEDIA = {MEDIA :N1}");
+          //  }
+          //  else 
+          //  {
+          //    Console.WriteLine("Presentation Error");
+          //  }
+            
+            //Console.ReadKey();
+        }
+    }
+}
+
+//Aparentemente na plataforma da DIO deu certo.
