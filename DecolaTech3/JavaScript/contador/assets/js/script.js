376d7245eae6bce60b7aaa46c631fa3b18cbed5f
Pedrobolfute <pepenike-101@hotmail.com>
2022-06-11 09:16:33 -0300

    document.body.style.backgroundColor added

--- DecolaTech3/JavaScript/contador/assets/js/script.js
+++ DecolaTech3/JavaScript/contador/assets/js/script.js
@@ -4,9 +4,15 @@ var currentNumber = 0;
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber >= 0){
+        document.body.style.backgroundColor = "rgb(32, 31, 31)"
+    }
 }
 
 function decrement(){
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber < 0){
+        document.body.style.backgroundColor = "rgb(130, 33, 33)"
+    }
 }
