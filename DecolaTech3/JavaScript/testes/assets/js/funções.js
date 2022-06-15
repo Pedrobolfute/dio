0284089ebaa32307359539d860524e7caa9cebbb
Pedrobolfute <pepenike-101@hotmail.com>
2022-06-15 16:28:09 -0300

    funções

---
+++ DecolaTech3/JavaScript/testes/assets/js/funções.js
@@ -0,0 +1,11 @@
+//função
+function funcao(){
+    console.log("WOor");
+}
+funcao();
+
+//funções com parametros //os parâmetros são como variáveis de estados
+function menssagem(parametro1, parametro2){
+    console.log(parametro1, parametro2)
+}
+menssagem("Menssagem do 1", "e 2 parametro");
