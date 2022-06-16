77424b5d2605562a0da5719606c4ec463a7cd8d7
Pedrobolfute <pepenike-101@hotmail.com>
2022-06-16 13:20:09 -0300

    operadores add

---
+++ DecolaTech3/JavaScript/testes/assets/js/operadores.js
@@ -0,0 +1,52 @@
+//ATRIBUIÇÃO
+/* 
+=   é atribuição
++=  (ex: x += y é equivalente a x = x+y)
+*=  (ex: X *= Y é equivalente a x = x*y)
+/=  (ex: x /= y é equivalente a x = x/y)
+%=  (ex: x %= y é equivalente a x = x%y)
+*/
+
+// OPERADORES ARITIMÁTICOS
+/* 
++   adição
+-   subtração
+*   multiplicação
+**  exponenciação
+/   divisão
+%   modulo
+++  incrementar
+--  decrementar
+*/
+
+//COMPARAÇÃO
+/* 
+==  igual a (2 == "2": true)
+=== mesmo valor e mosmo tipo (2 === "2": false)
+!=  diferente
+!== (valores e tipos diferentes)
+>   maior que
+<   menor que
+>=  maior ou igual
+<=  menor ou igual
+*/
+
+//OPERADORES DE LÓGICA
+/* 
+&&  "e" lógico
+||  "ou" lógico
+!   "não" lógico
+*/
+
+//CONDICIONAL VARIÁVEIS E TIPO
+//CONDICIONAL TERNÁRIO
+/* 
+(condicional) ? expressão1 : expressão2
+É IGUAL A
+if(condicional){
+    expressão1
+}
+else{
+    expressão2
+}
+*/
