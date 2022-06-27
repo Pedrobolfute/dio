236e6aa9b2bdb0419ca5e94fc073645298a48a97
Pedrobolfute <pepenike-101@hotmail.com>
2022-06-27 11:24:27 -0300

    acontecendo: criado const, settando atributtes, jogando em container, voltando para const global...

---
+++ DecolaTech3/JavaScript/todolist/assets/js/script.js
@@ -0,0 +1,33 @@
+const formulario = document.getElementById("formulario");
+const tarefas = document.getElementById("tarefas");
+
+formulario.onsubmit = function(e){
+    e.preventDefault();
+    const campoDeEntrada = document.getElementById("inserir");
+    novaTarefa(campoDeEntrada.value); //voltar nessa value.
+    formulario.reset();
+};
+
+function novaTarefa(descricao){
+    //const divFormulario = document.getElementById("tarefas");
+    const criarDiv = document.createElement("div");
+    const criarConteudo = document.createElement("input");
+    const criarlabel = document.createElement("label");
+    const descricaotxt = document.createTextNode(descricao);
+
+    criarConteudo.setAttribute("type", "checkbox");
+
+    criarDiv.classList.add("formulario-item");
+    criarDiv.appendChild(criarConteudo);
+
+    
+    tarefas.appendChild(criarDiv);
+
+    
+}
+
+
+
+function alerta(){
+    document.body.style.backgroundColor = "rgb(130, 33, 33)";
+}
